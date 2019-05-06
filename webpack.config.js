90ee87ded01d317352d02e2e93fdf130c652f480
cjdd <459953103@qq.com>
2019-05-06 17:59:04 +0800

    :zap: 更改了build配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,48 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
-module.exports = (env, arg) => ({
-  mode: env,
-  entry: './src/index.js',
-  devtool: env === 'development' ? 'cheap-module-eval-source-map' : false,
-  output: {
-    path: path.resolve(__dirname, './dist/'),
-    filename: 'pullRefresh.js',
-    library: 'PullRefresh',
-    libraryTarget: 'umd',
-    libraryExport: 'default'
-  },
-  devServer: {
-    inline: true,
-    port: 3000,
-    host: '192.168.1.169',
-    hot: true
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_mudules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
+module.exports = (env, arg) => {
+  const webpackConfig = {
+    mode: env,
+    entry: './src/index.js',
+    devtool: env === 'development' ? 'cheap-module-eval-source-map' : false,
+    output: {
+      path: path.resolve(__dirname, './dist/'),
+      filename: 'pullRefresh.js',
+      library: 'PullRefresh',
+      libraryTarget: 'umd',
+      libraryExport: 'default'
+    },
+   
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_mudules/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }
+
+  if (env === 'development') {
+    webpackConfig.devServer = {
+      inline: true,
+      port: 3000,
+      host: '192.168.1.169',
+      hot: true
+    }
+    webpackConfig.plugins.push(new HtmlWebpackPlugin({
+      title: '下拉加载',
       template: './index.ejs',
       filename: path.resolve(__dirname, './dist/index.html'),
       js: ['pullRefresh.js'],
-      inject: false
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
-})
+      inject: false,
+    }))
+  }
+  return webpackConfig
+}
